ff9b2ca94e5deb6cf7b770ff30bc140c87b7d5fe
Karin Ohman <kaohma@gmail.com>
2019-02-23 16:00:46 -0700

    Add thunks tests:

---
+++ src/thunks/__tests__/fetchCities.test.js
@@ -0,0 +1,55 @@
+import { fetchCities } from '../fetchCities';
+import { setLoading, setError, setCities } from '../../actions';
+import { fetchData } from '../../utils/api';
+jest.mock('../../utils/api');
+
+describe('fetchCities', () => {
+  let mockDispatch;
+
+  beforeEach(() => {
+    mockDispatch = jest.fn();
+  });
+
+  it('should call dispatch with the setLoading action', () => {
+    const thunk = fetchCities();
+    thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setLoading(true));
+  });
+
+  it('should call fetchData with the correct params', async () => {
+    const thunk = fetchCities();
+    await thunk(mockDispatch);
+    expect(fetchData).toHaveBeenCalledWith('http://api.citybik.es/v2/networks');
+  });
+
+  it('should dispatch setError with message if response is not ok', async () => {
+    fetchData.mockImplementation(() => {
+      throw 'Error fetching data'
+    });
+    const thunk = fetchCities();
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setError('Error fetching data'));
+  });
+
+  it('should dispatch setCities if response is ok', async () => {
+    const expected = { networks: [
+      { name: 'Denver', stations: 26, id: 'denver' },
+      { name: 'Capital Bikeshare', stations: 58, id: 'capital-bikeshare' }
+    ]};
+    fetchData.mockImplementation(() => expected);
+    const thunk = fetchCities();
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setCities(expected.networks));
+  });
+
+  it('should dispatch setLoading(false)', async () => {
+    const expected = { networks: [
+      { name: 'Denver', stations: 26, id: 'denver' },
+      { name: 'Capital Bikeshare', stations: 58, id: 'capital-bikeshare' }
+    ]};
+    fetchData.mockImplementation(() => expected);
+    const thunk = fetchCities();
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setLoading(false));
+  });
+});
---
+++ src/thunks/__tests__/fetchStations.test.js
@@ -0,0 +1,78 @@
+import { fetchStations } from '../fetchStations';
+import { setLoading, setError, setStations, setCurrentCity } from '../../actions';
+import { fetchData } from '../../utils/api';
+jest.mock('../../utils/api');
+
+describe('fetchStations', () => {
+  let mockDispatch;
+  let mockCity
+
+  beforeEach(() => {
+    mockCity = 'Denver';
+    mockDispatch = jest.fn();
+  });
+
+  it('should call dispatch with the setLoading action', () => {
+    const thunk = fetchStations(mockCity);
+    thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setLoading(true));
+  });
+
+  it('should call fetchData with the correct params', async () => {
+    const thunk = fetchStations(mockCity);
+    await thunk(mockDispatch);
+    expect(fetchData).toHaveBeenCalledWith(`http://api.citybik.es/v2/networks/${mockCity}`);
+  });
+
+  it('should dispatch setError with message if response is not ok', async () => {
+    fetchData.mockImplementation(() => {
+      throw 'Error fetching data'
+    });
+    const thunk = fetchStations(mockCity);
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setError('Error fetching data'));
+  });
+
+  it('should dispatch setStations if response is ok', async () => {
+    const expected = {
+      network: [
+        { name: 'Denver', stations: 26, id: 'denver' },
+        { name: 'Capital Bikeshare', stations: 58, id: 'capital-bikeshare' }
+      ]
+    };
+    fetchData.mockImplementation(() => expected);
+    const thunk = fetchStations(mockCity);
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setStations(expected.networks));
+  });
+
+  it('should dispatch setCurrentCity if response is ok', async () => {
+    const expected = {
+      network: {
+        stations: [
+          { name: '1st & Broadway', bikes_available: 5, id: '1' },
+          { name: '18th & Blake', bikes_available: 8, id: '2' }
+        ]
+      }
+    };
+    fetchData.mockImplementation(() => expected);
+    const thunk = fetchStations(mockCity);
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setCurrentCity(mockCity));
+  });
+
+  it('should dispatch setLoading(false)', async () => {
+    const expected = {
+      network: {
+        stations: [
+          { name: '1st & Broadway', bikes_available: 5, id: '1' },
+          { name: '18th & Blake', bikes_available: 8, id: '2' }
+        ]
+      }
+    };
+    fetchData.mockImplementation(() => expected);
+    const thunk = fetchStations(mockCity);
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(setLoading(false));
+  });
+});
