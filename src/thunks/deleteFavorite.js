690fb62f9ed4248956adb57e1df3dcecb2f8fb7b
Karin Ohman <kaohma@gmail.com>
2019-03-11 12:36:09 -0600

    Fix bug with popup flashing

--- src/thunks/deleteFavorite.js
+++ src/thunks/deleteFavorite.js
@@ -1,15 +1,13 @@
-import { removeFavorite, setLoading, setError } from '../actions';
+import { removeFavorite, setError } from '../actions';
 import { fetchData } from '../utils/api';
 
 export const deleteFavorite = (station_id, user_id) => {
   return async (dispatch) => {
     try {
-      // dispatch(setLoading(true));
       await fetchData(`https://bike-see.herokuapp.com/api/v1/users/${user_id}/favorites/${station_id}`, 'DELETE');
       dispatch(removeFavorite(station_id));
     } catch (error) {
       dispatch(setError('Error deleting favorite.'));
     }
-    // dispatch(setLoading(false));
   }
 }
--- src/thunks/postFavorite.js
+++ src/thunks/postFavorite.js
@@ -1,15 +1,13 @@
-import { addFavorite, setLoading, setError } from '../actions';
+import { addFavorite, setError } from '../actions';
 import { fetchData } from '../utils/api';
 
 export const postFavorite = (station_id, user_id) => {
   return async (dispatch) => {
     try {
-      // dispatch(setLoading(true));
       await fetchData(`https://bike-see.herokuapp.com/api/v1/users/favorites/new`, 'POST', { user_id, station_id});
       dispatch(addFavorite(station_id));
     } catch (error) {
       dispatch(setError('Error adding favorite.'));
     }
-    // dispatch(setLoading(false));
   }
 }
