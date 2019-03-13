6bcd3b6c47b7505b27c90f56bf2081a08fd18927
Karin Ohman <kaohma@gmail.com>
2019-03-13 15:56:26 -0600

    Update post favorite path to match back end

--- src/thunks/postFavorite.js
+++ src/thunks/postFavorite.js
@@ -4,7 +4,7 @@ import { fetchData } from '../utils/api';
 export const postFavorite = (station_id, user_id) => {
   return async (dispatch) => {
     try {
-      await fetchData(`https://bike-see.herokuapp.com/api/v1/users/favorites/new`, 'POST', { user_id, station_id});
+      await fetchData(`https://bike-see.herokuapp.com/api/v1/users/favorites`, 'POST', { user_id, station_id});
       dispatch(addFavorite(station_id));
     } catch (error) {
       dispatch(setError('Error adding favorite.'));
